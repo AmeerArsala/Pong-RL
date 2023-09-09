7c8a64f96395abb4fd94a42584e1fc6f90f9400a
Ameer Arsala <ameer.arsala03@gmail.com>
2023-09-09 16:06:39 -0700

    added scoreboard + more documentation

--- Assets/Source/Scripts/GameManager.cs
+++ Assets/Source/Scripts/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //public int number = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //i++;
+        //Debug.Log("Hello World! x" + i);
     }
 }
